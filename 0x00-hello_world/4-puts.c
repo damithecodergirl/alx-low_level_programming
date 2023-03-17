e9181a1c46ca421561a5cbf3388f5ba8a0e2d048
damithecodergirl <oluwadamilolaojo1810@gmail.com>
2023-03-17 03:46:00 +0000

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
-* Description
-* main: prints out string to stdout
-* Return: returns the value 0 if successful
+* Description: main- prints out string to stdout.
+* Return: returns the value 0 if successful.
 */
 
 int main(void)
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*Description: main- prints string to stdout
+*return: returns value 0 if successful
+*/
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
