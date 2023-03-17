e4b08db35d850a2c971ecec4d98cb8fd0c0d36d4
damithecodergirl <oluwadamilolaojo1810@gmail.com>
2023-03-17 03:35:52 +0000

    puts..c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* Description
+* main: prints out string to stdout
+* Return: returns the value 0 if successful
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
