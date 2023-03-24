67d98e8455df8a34cf5ef70a0a888c43902bcf44
damithecodergirl <oluwadamilolaojo1810@gmail.com>
2023-03-24 01:42:54 +0000

    A program that prints 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - prints 0 to 9
+* Return: 0 if successful
+*/
+
+int main(void)
+{
+	for (char n = '0'; n <= '9'; n++)
+	{
+	putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
