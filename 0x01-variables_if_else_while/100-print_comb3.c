31b9aca70faf6586272428c9f61b82663f2f97da
damithecodergirl <oluwadamilolaojo1810@gmail.com>
2023-03-26 03:53:48 +0000

    Wrote a code that prints combinations of two numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - prints the combinations of two digits without repition
+* Return: 0 if successful
+*/
+
+int main(void)
+{
+	int c1;
+	int c2;
+
+	for (c1 = '0'; c1 <= '9'; c1++)
+	{
+		for (c2 = c1 + 1; c2 <= '9'; c2++)
+		{
+		putchar(c1);
+		putchar(c2);
+
+		if (c1 != '8' || c2 != '9')
+		{
+		putchar(',');
+		putchar(' ');
+		}
+
+		}
+	}
+	
+	putchar('\n');
+	return (0);
+}
