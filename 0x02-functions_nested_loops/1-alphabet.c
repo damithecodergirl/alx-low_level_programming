a58e49156e00e587a010a3878d0046060a18bace
damithecodergirl <oluwadamilolaojo1810@gmail.com>
2023-04-09 05:54:10 +0000

    Wrote a codde that prints lower case alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,15 @@
 
 /**
 * main - prints lower case with a prototype function in "main.h"
-* Return: 0 if successful 
 */
+
 
 void print_alphabet(void)
 {
-	for (char ch = 'a'; ch <= 'z'; ch++)
+char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 	_putchar(ch);
 	}
 	_putchar('\n');
-
-	return(0);
 }
